47b1ad056285a606eb1d7ba8f984d5c49f201400
Matthew Hammond <mj.hammond@hotmail.co.uk>
2018-03-28 23:49:52 +0100

    found out about lcm :>

--- 5.hs
+++ 5.hs
@@ -1,8 +1,2 @@
-divides :: Integer -> Integer -> Bool
-divides x y = x `mod` y == 0
-
-divisible :: [Integer] -> Integer -> Bool
-divisible xs y = all (== True) (map (divides y) xs)
-
 main = do
-  putStrLn $ show $ (length $ takeWhile (== False) $ map (divisible [1..20]) [1..]) + 1
+  putStrLn $ show $ foldr1 lcm [1..20]
