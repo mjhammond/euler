47bd1ca0def9ede1ad538cc9a72774c0cf8c3e01
Matthew Hammond <mj.hammond@hotmail.co.uk>
2018-03-30 20:47:39 +0100

    using a self-referencing primes test

--- 7.hs
+++ 7.hs
@@ -1,17 +1,15 @@
-divides :: Integer -> Integer -> Bool
-divides d n = rem n d == 0
-
-ld :: Integer -> Integer -> Integer
-ld k n | divides k n = k
-        | k^2 > n = n
-        | otherwise = ld (k+1) n
+ld :: [Integer] -> Integer -> Integer
+ld (p:ps) n | rem n p == 0 = p
+            | p^2 > n = n
+            | otherwise = ld ps n
 
 prime :: Integer -> Bool
 prime n | n < 1 = error "not a positive integer"
         | n == 1 = False
-        | otherwise = ld 2 n == n
+        | otherwise = ld primes n == n
 
-primes = filter prime [2..]
+primes :: [Integer]
+primes = 2 : filter prime [3..]
 
 main = do
   putStrLn $ show $ primes !! 10000
